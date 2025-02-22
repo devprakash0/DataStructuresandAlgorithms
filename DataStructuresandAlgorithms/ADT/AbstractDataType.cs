73818beaed952429d40a513cca3eabb8939afa08
Amit Prakash <amitbcet2k15@gmail.com>
2025-02-22 15:18:29 +0530

    Abstract Data Type

---
+++ DataStructuresandAlgorithms/ADT/AbstractDataType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresandAlgorithms.ADT
+{
+    public class AbstractDataType
+    {
+        public string Name { get; set; }
+
+        public int Exp { get; set; }
+
+        public bool IsAbstractDataType( )
+        {
+            return true;
+        }
+    }
+}
--- DataStructuresandAlgorithms/Program.cs
+++ DataStructuresandAlgorithms/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace DataStructuresandAlgorithms
+﻿using DataStructuresandAlgorithms.ADT;
+using System.Collections;
+
+namespace DataStructuresandAlgorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            // Line#12 and Line#13 are doing the same thing interally
+            Stack stack = new Stack();
+
+            AbstractDataType adt = new AbstractDataType();
+
             Console.WriteLine("Hello, World!");
         }
     }
